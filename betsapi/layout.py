20b4faaf77c608895b17a66c6a123b255e3a4eeb
Marcus Holanda <mvsh777@hotmail.com>
2024-07-29 12:51:18 -0300

    informar liga

--- betsapi/layout.py
+++ betsapi/layout.py
@@ -21,30 +21,50 @@ como partidas, horários, odds e outros dados relevantes, utilizando técnicas d
 
 ---
 
+## Instalação
+
+```bash
+pip install "git+https://github.com/Marcus-Holanda777/betsapi.git"
+```
+
 ## Como usar ?
 
 ```diff
-- O site exige autenticação e gerenciamento de cookies, por isso a cada execução será gerado um novo arquivo `json` com o `Headers` atualizado.
+- O site exige autenticação e gerenciamento de cookies. 
+- A cada execução será gerado um novo arquivo `json` com o `Headers` atualizado.
 ```
 
 Para usar, basta chamar o app `betsapi`, e depois digitar os comandos.
 
 1. init
 > Faz a configuração inicial do programa.
-```js
-init options: --version -v, --help -h
+```bash
+init options: --version -v, --obscure -o,--help -h
+
+Digite seu email ✉: teste@init.com
+Digite sua senha 😎:
 ```
 
 2. page
-> Exporta os dados a partir de um intervalo de `paginas`.
-```js
+> Exporta os dados a partir de uma liga e intervalo de paginas `paginas`.
+```bash
 page options: --start -s, --end -e, --version -v, --help -h
 ```
 
+```bash
+betsapi page "1234/Futebol" -s 1 -e 10
+```
+
 3. link
-> Exporta os dados a partir de um link.
-```js
+> Exporta os dados a partir de um link ou uma planilha.
+```bash
 link args: url [required], options: --version -v, --help -h
+```
+
+```bash
+betsapi link "c:\links.xlsx"
+
+betsapi link "https://url"
 ```
 
 Des. Marcus Holanda
